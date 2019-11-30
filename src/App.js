da082fc577033bab6750472b20e4e94be9776ce2
aplimh <iap.aplimh@gmail.com>
2019-11-30 14:31:23 +0200

    Varianta dupa lab. 7

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import LOperatori from "./operatori";
 import CorpTabel from "./corptabel";
-import Formular from "./formular1";
+import Formular from "./formular";
 import "./App.css";
 
 class App extends Component {
@@ -9,77 +9,72 @@ class App extends Component {
     super(props);
     this.state = {
       operatori: LOperatori,
-      oper: { id: -1, nume: "", numePrenume: "", email: "", locatie: "" }
+      opedit: { id: 0, nume: "", numePrenume: "", email: "", locatie: "" },
+      cheie: 0
     };
-    this.adaugOper = this.adaugOper.bind(this);
-    this.corectezOpTabel = this.corectezOpTabel.bind(this);
-    this.stergOper = this.stergOper.bind(this);
-    this.corectezOpForm = this.corectezOpForm.bind(this);
-  }
 
-  //  In tabel s-a selectat editare
-  corectezOpTabel(ev) {
-    const idSup = parseInt(ev.target.id); //  id e convertit in intreg
-    console.log(`App oper id: ${idSup}`);
-    //  Preiau in state.oper datele operatorului
-    const op = this.state.operatori;
-    let oper;
-    for (let x in op) {
-      if (op[x].id === idSup) {
-        oper = op[x];
-        break;
-      }
-    }
-    console.log(`App oper nume: ${oper.nume}`);
-    this.setState({
-      operatori: this.state.operatori,
-      oper: oper
-    });
-    const opp = this.state.oper;
-    console.log(`App opp keys: ${Object.keys(opp)}`);
+    this.stergOper = this.stergOper.bind(this);
+    this.corectezOper = this.corectezOper.bind(this);
+    this.formSubmit = this.formSubmit.bind(this);
   }
 
   //  In tabel s-a selectat stergere
   stergOper(ev) {
-    const idSup = parseInt(ev.target.id); //  id e convertit in intreg
     const { operatori } = this.state;
     const sirNou = operatori.filter(item => {
-      return item.id !== idSup;
+      return parseInt(item.id) !== parseInt(ev.target.id);
       //  Obiectul care are id === idSup nu se copiaza in noul sir
     });
     this.setState({
-      operatori: sirNou,
-      oper: { id: -1, nume: "", numePrenume: "", email: "", locatie: "" }
+      operatori: sirNou
     });
   }
 
-  //  In formular s-a selectat submit si era o editare
-  corectezOpForm(oper) {
-    //  Inlocuiesc in state datele operatorului
-    console.log(`   in corectezOper oper.nume: ${oper.nume}`);
-    const op = this.state.operatori;
-    const opnou = op.map(item => {
-      if (item.id === oper.id) {
-        return oper;
-      } else {
-        return item;
-      }
+  //  In tabel s-a selectat editare
+  corectezOper(ev) {
+    //  Caut operatorul in lista operatorilor
+    const idOper = parseInt(ev.target.id);
+    const lOperatori = this.state.operatori;
+    const oper = lOperatori.find(item => {
+      return parseInt(item.id) === idOper;
     });
     this.setState({
-      operatori: opnou,
-      oper: { id: -1, nume: "", numePrenume: "", email: "", locatie: "Alba" }
+      operatori: lOperatori,
+      opedit: oper,
+      cheie: idOper //  Important!!! Provoaca reconstruirea componentei "Formular"
     });
   }
 
-  adaugOper(oper) {
-    // Voi preiau datele din fomular si creez un obiect nou
-    const op = [...this.state.operatori, oper];
-    this.setState({ operatori: op });
+  //  In formular s-a selectat submit
+
+  formSubmit(oper) {
+    const idOper = parseInt(oper.id);
+    let op;
+    if (parseInt(oper.id) === 0) {
+      //  Este o adaugare
+      oper.id = 1000; //  De fapt va fi un id adevarat, venit din baza de date!
+      op = [...this.state.operatori, oper];
+    } else {
+      //  Este o editare
+      const opEd = this.state.operatori;
+      op = opEd.map(item => {
+        if (parseInt(item.id) === idOper) {
+          return oper;
+        } else {
+          return item;
+        }
+      });
+    }
+    //  Refac "state"
+    this.setState({
+      operatori: op,
+      opedit: { id: 0, nume: "", numePrenume: "", email: "", locatie: "" },
+      cheie: 0
+    });
   }
 
   render() {
     //  in Formular folosesc this.props.edit (contact de editat)
-
     return (
       <div className="container">
         <h2 className="text-center mt-5 mb-3">
@@ -98,15 +93,16 @@ class App extends Component {
           <tbody>
             <CorpTabel
               operatori={this.state.operatori}
-              corectezOpTabel={this.corectezOpTabel}
               stergOper={this.stergOper}
+              corectezOper={this.corectezOper}
             />
           </tbody>
         </table>
+        <hr></hr>
         <Formular
-          corectezOpForm={this.corectezOpForm}
-          adaugOper={this.adaugOper}
-          oper={this.state.oper}
+          key={this.state.cheie}
+          oper={this.state.opedit}
+          formSubmit={this.formSubmit}
         />
       </div>
     );
