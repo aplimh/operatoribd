27bd7de4d6152f7b1ad710df67c7c9b2e8a77163
aplimh <iap.aplimh@gmail.com>
2019-12-14 21:09:41 +0200

    Reconstruit

--- src/App.js
+++ src/App.js
@@ -21,41 +21,54 @@ class App extends Component {
   reincarc() {
     //  reincarc lista de contacte din baza de date
 
-    fetch("http://localhost/operatoribd/api/listaop.php")
-      .then(rezultat => {
-        return rezultat.json();
-      })
-      .then(lista => {
-        this.setState({ operatori: lista });
-      })
-      .catch(error => console.log("Request failed", error));
+    return (
+      fetch("http://localhost/operatoribd/api/listaop.php")
+        .then(rezultat => {
+          return rezultat.json();
+        })
+        //  .then(lista => {
+        //    this.setState({ operatori: lista });
+        //  })
+        .catch(error => console.log("Request failed", error))
+    );
   }
 
   componentDidMount() {
-    this.reincarc();
+    this.reincarc().then(lista => {
+      this.setState({ operatori: lista });
+    });
   }
 
   //  In tabel s-a selectat stergere
   stergOper(ev) {
-    const idSup = ev.target.id;
-    //  Construiesc un obiect FormData
-    const formData = new FormData();
-    formData.append("id", parseInt(idSup));
+    //  Construiesc sirul de caractere care se trimite scriptului PHP
+    const dateScript = `id=${ev.target.id}`;
+    const config = {
+      method: "POST",
+      headers: { "Content-type": "application/x-www-form-urlencoded" },
+      body: dateScript // body data type must match "Content-Type" header
+    };
+
     //  Corectez in baza de date
-    fetch("http://localhost/operatoribd/api/delop.php", {
-      body: formData,
-      method: "post"
-    }).then(this.reincarc);
+    fetch("http://localhost/operatoribd/api/delop.php", config)
+      .then(this.reincarc) //  this.reincarc a returnat lista decodficata
+      .then(lista => this.setState({ operatori: lista }));
   }
 
   //  In tabel s-a selectat editare
   corectezOper(ev) {
     //  Caut operatorul in lista operatorilor
-    const idOper = parseInt(ev.target.id);
+    let idOper = parseInt(ev.target.id);
     const lOperatori = this.state.operatori;
     const oper = lOperatori.find(item => {
       return parseInt(item.id) === idOper;
     });
+    console.log(`idOper: ${idOper}`);
+    if (idOper == this.state.cheie) {
+      //  Voi corecta ultimul articol introdus. Pun cheie pe 0!
+      idOper = 0;
+    }
+
     this.setState({
       opedit: oper,
       cheie: idOper //  Important!!! Provoaca reconstruirea componentei "Formular"
@@ -65,46 +78,59 @@ class App extends Component {
   //  In formular s-a selectat submit
 
   formSubmit(oper) {
-    //  Construiesc un obiect FormData
-    const formData = new FormData();
-    formData.append("id", parseInt(oper.id));
-    formData.append("nume", oper.nume);
-    formData.append("numePrenume", oper.numePrenume);
-    formData.append("email", oper.email);
-    formData.append("locatie", oper.locatie);
+    const dateScript =
+      `id=${parseInt(oper.id)}` +
+      `&nume=${oper.nume}` +
+      `&numePrenume=${oper.numePrenume}` +
+      `&email=${oper.email}` +
+      `&locatie=${oper.locatie}`;
+    const config = {
+      method: "POST",
+      headers: { "Content-type": "application/x-www-form-urlencoded" },
+      body: dateScript // body data type must match "Content-Type" header
+    };
     if (parseInt(oper.id) === 0) {
       //  Este o adaugare. Incarc contactul in baza de date
-      fetch("http://localhost/operatoribd/api/adaop.php", {
-        body: formData,
-        method: "post"
-      })
+      const url = "http://localhost/operatoribd/api/adaop.php";
+      let id = 0;
+      fetch(url, config)
         .then(rezultat => {
           return rezultat.json();
         })
-        .then(lista => {
-          this.setState({
-            opedit: {
-              id: 0,
-              nume: "",
-              numePrenume: "",
-              email: "",
-              locatie: ""
-            },
-            cheie: lista[1].id
-          });
-          this.reincarc();
+        .then(raspuns => {
+          id = raspuns[1].id;
         });
+      this.reincarc().then(lista => {
+        this.setState({
+          operatori: lista,
+          opedit: {
+            id: 0,
+            nume: "",
+            numePrenume: "",
+            email: "",
+            locatie: ""
+          },
+          cheie: id
+        });
+      });
     } else {
       //  Este o editare
-      fetch("http://localhost/operatoribd/api/modiop.php", {
-        body: formData,
-        method: "post"
-      }).then(() => {
+      const url = "http://localhost/operatoribd/api/modiop.php";
+      fetch(url, config).then(rezultat => {
+        return rezultat.json();
+      });
+      this.reincarc().then(lista => {
         this.setState({
-          opedit: { id: 0, nume: "", numePrenume: "", email: "", locatie: "" },
+          operatori: lista,
+          opedit: {
+            id: 0,
+            nume: "",
+            numePrenume: "",
+            email: "",
+            locatie: ""
+          },
           cheie: 0
         });
-        this.reincarc();
       });
     }
   }
